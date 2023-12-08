3b9f39d5670a44eb8fd31f4fcc48b4fe3164974e
blandfi <75065714+blandfi@users.noreply.github.com>
2023-12-08 18:07:00 +0100

    Add files via upload

---
+++ libft/ft_strrev.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfiguet <bfiguet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 18:12:52 by bfiguet           #+#    #+#             */
+/*   Updated: 2023/11/21 12:51:37 by bfiguet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrev(char *str)
+{
+	int		len;
+	int		i;
+	char	tmp;
+
+	len = 0;
+	i = 0;
+	while (str[len])
+		len++;
+	len -= 1;
+	while (i < len)
+	{
+		tmp = str[i];
+		str[i] = str[len];
+		str[len] = tmp;
+		i++;
+		len--;
+	}
+	return (str);
+}
